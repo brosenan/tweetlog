b0168c27f44184c2425f783ce41f75a492d1a466
Boaz Rosenan <brosenan@gmail.com>
2015-12-16 18:10:08 +0200

    cloudlog.addAxiom - happy path

--- app/components/clg/clg.js
+++ app/components/clg/clg.js
@@ -5,4 +5,25 @@ angular.module('cloudlog', [
   'cloudlog-directive'
 ])
 
-.value('version', '0.1');
+    .value('version', '0.1')
+    .factory('cloudlog', ['$http', '$log', function($http, $log) {
+	return {
+	    addAxioms: function(pattern, data) {
+		function handleError(resp) {
+		}
+		$http({
+		    method: 'POST',
+		    url: '/encode/f',
+		    data: pattern,
+		    headers: {'content-type': 'text/cloudlog'},
+		}).then(function(resp) {
+		    var url = resp.data.url;
+		    $http({
+			method: 'POST',
+			url: url,
+			data: data,
+		    }).then(function(resp) {}, handleError);
+		}, handleError);
+	    },
+	};
+    }]);
--- app/components/clg/clg_test.js
+++ app/components/clg/clg_test.js
@@ -1,11 +1,32 @@
 'use strict';
 
 describe('cloudlog module', function() {
-  beforeEach(module('cloudlog'));
+    beforeEach(module('cloudlog'));
+    var $httpBackend;
+    var cloudlog;
+    beforeEach(inject(function($injector) {
+	$httpBackend = $injector.get('$httpBackend');
+//	encodeF = $httpBackend.when('POST', '/encode/f')
+//            .respond({userId: 'userX'}, {'A-Token': 'xxx'});
+	cloudlog = $injector.get('cloudlog');
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
     }));
-  });
+
+    afterEach(function() {
+	$httpBackend.verifyNoOutstandingExpectation();
+	$httpBackend.verifyNoOutstandingRequest();
+    });
+
+    describe('.addAxioms(pattern, data)', function(){
+	it('should encode the pattern and then use the given URL', function(){
+	    var pattern = "foo(A, B)";
+	    var data = [{A: 1, B: 2}];
+	    $httpBackend.expectPOST('/encode/f', pattern).respond(200, '{"url":"http://myserver/abcdefg123"}');
+	    $httpBackend.expectPOST('http://myserver/abcdefg123', data).respond(200, '{"status":"OK"}');
+	    cloudlog.addAxioms(pattern, data);
+	    $httpBackend.flush();
+	});
+
+    });
+
 });
